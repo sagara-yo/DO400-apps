ed332d2d08e9fed894dd69e72cfb680a1fe3dc04
Yoshihiro Sagara <sagara.yoshihiro@nttcom.co.jp>
2022-11-24 01:05:24 -0500

    Refactor

--- school-library/src/test/java/com/redhat/training/BookStatsTest.java
+++ school-library/src/test/java/com/redhat/training/BookStatsTest.java
@@ -7,20 +7,17 @@ import com.redhat.training.books.Book;
 import org.junit.jupiter.api.Test;
 
 public class BookStatsTest {
-
-    @Test
-    public void countingWordsOfEmptyBookReturnsZero() {
-        // Given
-        Book book = new Book("someISBN");
-
-        // When
-        double wordCount = BookStats.countWords(book);
-
-        // Then
-        assertEquals(0, wordCount);
+@Test
+public void countingWordsReturnsNumberOfWordsInBook() {     
+// Given
+    Book book = new Book("someISBN", "this is the content");
+// When
+    double wordCount = BookStats.countWords(book);
+// Then
+    assertEquals(4, wordCount);
     }
 
-    @Test
+@Test
     public void countingWordsReturnsNumberOfWordsInBook() {
         assertEquals(0, 1); // Replace this line with the actual test code...
     }
--- school-library/src/test/java/com/redhat/training/LibraryTest.java
+++ school-library/src/test/java/com/redhat/training/LibraryTest.java
@@ -22,6 +22,21 @@ public class LibraryTest {
         inventory = new InMemoryInventory();
         library = new Library(inventory);
     }
-
-    // Add tests here...
+@Test
+public void checkingOutUnavailableBookThrowsException()
+throws BookNotAvailableException {
+// Given
+inventory.add(new Book("book1"));
+inventory.add(new Book("book1"));
+library.checkOut("student1", "book1");
+library.checkOut("student2", "book1");
+// When
+final BookNotAvailableException exception = assertThrows(BookNotAvailableException.class,
+() -> {
+library.checkOut("student3", "book1");
+}
+);
+// Then
+assertTrue(exception.getMessage().matches("Book book1 is not available"));
+}
 }
